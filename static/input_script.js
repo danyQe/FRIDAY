fcf934b508e52255fcf71beda1fc5bfcef5b183a
danyQe <71999062+danyQe@users.noreply.github.com>
2024-08-24 09:25:11 +0530

    Update input_script.js

--- static/input_script.js
+++ static/input_script.js
@@ -341,7 +341,7 @@ then(response => {
                         if (!response.ok) {
                             throw new Error('Network response was not ok');
                         }
-                        return response.text();
+                        return response.json();
                     })
                     .then(data => {
                         if (data.message) {
